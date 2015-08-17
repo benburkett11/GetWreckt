c01b184e63b09b0135346cd7775783085437238c
benburkett11 <benburkett11@gmail.com>
2015-08-16 21:40:12 -0400

    Added the functionality of the scraper.

--- index.php
+++ index.php
@@ -5,3 +5,49 @@
  * Date: 8/16/2015
  * Time: 8:46 PM
  */
+require_once 'vendor/autoload.php';
+use \Curl\Curl;
+
+set_time_limit( 300 );
+libxml_use_internal_errors(true);
+
+$count = 2;
+$countToQuit = 0;
+$link = 'http://getwreckt.com/home/insult/';
+$curl = new Curl();
+$dom = new DOMDocument();
+$bool = TRUE;
+
+$insults = array();
+
+while( $bool ){
+    $curl->get( $link . $count );
+    $dom->loadHTML( $curl->response );
+    $selector = new DOMXPath( $dom );
+    $result = $selector->query( '/html/body/header//h1' );
+    $insult = $result[0]->nodeValue;
+    if( isset( $insults[$insult] ) ){
+        $count++;
+        $countToQuit++;
+    } else {
+        $insults[$insult] = 1;
+        $count++;
+    }
+
+    if( $countToQuit > 20 )
+        break;
+
+}
+$curl->close();
+
+$output = fopen("insults.txt", "w");
+
+foreach( $insults as $key => $i )
+    fwrite( $output, $key . PHP_EOL );
+
+fclose( $output );
+echo count( $insults );
+
+
+
+?>
